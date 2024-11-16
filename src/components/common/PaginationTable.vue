a4b00051022bdfcd15bea11cc8315f555cdcf6d6
TruyenBD <mrtruyenbd1407@gmail.com>
2024-11-17 00:08:23 +0700

    feat: pagination event change current page

--- src/components/common/PaginationTable.vue
+++ src/components/common/PaginationTable.vue
@@ -2,7 +2,6 @@
 import {
   Button,
 } from '@/components/ui/button'
-
 import {
   Pagination,
   PaginationEllipsis,
@@ -14,30 +13,53 @@ import {
   PaginationPrev,
 } from '@/components/ui/pagination'
 
+import { ref, watch } from 'vue'
+
 interface Props {
   total: number
   currentPage: number
 }
 const props = defineProps<Props>()
-function hanleClick(item: { type: string, value: number }) {
-  console.log(item.value)
+
+const currentPage = ref(props.currentPage)
+
+function handlePageChange(newPage: number) {
+  currentPage.value = newPage
+  console.log('Navigated to page:', newPage)
 }
 
+watch(currentPage, (newVal) => {
+  console.log('Current page updated:', newVal)
+})
 </script>
 
 <template>
-  <Pagination v-slot="{ page }" :total="props.total" :sibling-count="1" show-edges :default-page="props.currentPage">
+  <Pagination
+    v-slot="{ page }"
+    :total="props.total"
+    :sibling-count="1"
+    show-edges
+    :default-page="props.currentPage"
+    @update:page="handlePageChange"
+  >
     <PaginationList v-slot="{ items }" class="flex items-center gap-1 justify-center">
       <PaginationFirst />
       <PaginationPrev />
 
-      <template v-for="(item, index) in items">
-        <PaginationListItem v-if="item.type === 'page'" :key="index" :value="item.value" as-child>
-          <Button class="w-10 h-10 p-0" :variant="item.value === page ? 'default' : 'outline'" @click="hanleClick(item)">
+      <template v-for="(item, index) in items" :key="index">
+        <PaginationListItem
+          v-if="item.type === 'page'"
+          :value="item.value"
+          as-child
+        >
+          <Button
+            class="w-10 h-10 p-0"
+            :variant="item.value === page ? 'default' : 'outline'"
+          >
             {{ item.value }}
           </Button>
         </PaginationListItem>
-        <PaginationEllipsis v-else :key="item.type" :index="index" />
+        <PaginationEllipsis v-else :index="index" />
       </template>
 
       <PaginationNext />
