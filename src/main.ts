4f9a18ca7b44ea53273a0b4138f06aaca810f299
TruyenBD <mrtruyenbd1407@gmail.com>
2024-11-21 16:53:13 +0700

    feat: UI dialog create blog

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
+import { QuillEditor } from '@vueup/vue-quill'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import '@vueup/vue-quill/dist/vue-quill.snow.css'
 import './assets/index.css'
 import './utils/zodLocale'
 
@@ -10,6 +12,8 @@ const app = createApp(App)
 app
   .use(createPinia())
   .use(router)
+
+app.component('QuillEditor', QuillEditor)
 
 router.isReady().then(() => {
   app.mount('#app')
--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -44,4 +44,3 @@ export const resetPasswordValidator = z
     message: 'Password confirmation does not match',
     path: ['confirmPassword'],
   })
-
